5ec8d7c8148a8f0e8d6830936f6c07bf5a084c6f
rimavebri <rmvbry@gmail.com>
2024-12-29 06:40:44 +0700

    chore: little enhancement on UI setting

--- lib/features/task/presentation/screen/settings.screen.dart
+++ lib/features/task/presentation/screen/settings.screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task_manager/core/constant/color.dart';
 import 'package:task_manager/features/task/presentation/bloc/theme/theme_bloc.dart';
 import 'package:task_manager/features/task/presentation/screen/widget/bottom_nav_bar.dart';
 
@@ -12,17 +14,22 @@ class SettingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: Center(
-        child: BlocBuilder<ThemeCubit, bool>(
-          builder: (context, isDarkMode) {
-            return SwitchListTile(
-              title: Text(isDarkMode ? 'Mode Gelap' : 'Mode Terang'),
-              value: isDarkMode,
-              onChanged: (value) {
-                context.read<ThemeCubit>().toggleTheme(value);
-              },
-            );
-          },
+      body: Padding(
+        padding: EdgeInsets.all(50.w),
+        child: Center(
+          child: BlocBuilder<ThemeCubit, bool>(
+            builder: (context, isDarkMode) {
+              return SwitchListTile(
+                title: Text(isDarkMode ? 'Mode Gelap' : 'Mode Terang'),
+                value: isDarkMode,
+                activeColor: AppColor.primaryColor,
+                inactiveTrackColor: AppColor.secondaryColor,
+                onChanged: (value) {
+                  context.read<ThemeCubit>().toggleTheme(value);
+                },
+              );
+            },
+          ),
         ),
       ),
       bottomNavigationBar: const BottomNavBar(
